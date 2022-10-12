1d0a88470819dea7f20f3d8b01c714156a81f968
VegiHarendranath33 <108779050+VegiHarendranath33@users.noreply.github.com>
2022-10-12 14:56:16 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var marks = 75;
-if (marks >80 )
-{
-  console.log('GRADE A');
-}
-else 
-{
+// var marks = 75;
+// if (marks >80 )
+// {
+//   console.log('GRADE A');
+// }
+// else 
+// {
+//     console.log('GRADE B');
+// }
+
+var marks = 40;
+ if (marks >80 )
+ {   console.log('GRADE A');
+ }
+  else if (marks > 55)
+  {
     console.log('GRADE B');
+  }
+ else 
+ {
+   console.log('GRADE C');
 }
